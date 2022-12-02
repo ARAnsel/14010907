ca101741b470a738cba389b7e23065c548438327
ARAnsel <118567005+ARAnsel@users.noreply.github.com>
2022-12-02 03:39:22 -0800

    Add files via upload

---
+++ heder.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Test site</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<header>
+    <div class="div1" >
+<img class="logo" src="http://localhost/Testsite/image/logo.png">
+<h1>Test shop</h1>
+<button onclick="document.location='login.php'" >login</button>
+    </div> 
+</header>
---
+++ register.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="login-box">
+        <h2>Register</h2> 
+    <form class="register" >
+       <div class="user" >
+
+       <label for="username" >username</label><br>
+        <input type="text" id="username" name="username"><br>
+        
+       </div>
+       <div class="user" >
+
+       <label for="pas" >Email</label><br>
+        <input type="text" id="pas" name="pas"><br>
+       
+       </div>
+       <div class="user" >
+
+<label for="username" >password</label><br>
+ <input type="text" id="username" name="username"><br>
+ 
+</div>
+<div class="user" >
+
+<label for="username" >password repeat</label><br>
+ <input type="text" id="username" name="username"><br>
+ 
+</div>
+       <div class="but">
+        <a id="submit" href="#">Submit</a>
+        <div class="regis" >
+</body>
